e16f1cb5be15ac615b6fa257f53610700e04b53e
PREETHIVIRAJ <20eumt057@skcet.ac.in>
2024-11-29 00:09:23 +0530

    Delete routes directory

--- routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from  "express";
-
-import { addProduct } from "../controllers/adminControllers.js";
-
-const router = express.Router();
-
-router.post("/add",addProduct )
-
-export default router;
